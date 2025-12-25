07fb680dad0d08e421eb3be1fec0f22dea9680c1
PordakSergey <pordakk@gmail.com>
2025-12-25 18:11:51 +0300

    add proto

---
+++ src/Grpc/CentrifugoProxy.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Rr\Bundle\Centrifugo\Grpc;
+
+use GRPC\Centrifugo\CentrifugoProxyInterface;
+use GRPC\Centrifugo\ConnectRequest;
+use GRPC\Centrifugo\ConnectResponse;
+use GRPC\Centrifugo\NotifyCacheEmptyRequest;
+use GRPC\Centrifugo\NotifyCacheEmptyResponse;
+use GRPC\Centrifugo\NotifyChannelStateRequest;
+use GRPC\Centrifugo\NotifyChannelStateResponse;
+use GRPC\Centrifugo\PublishRequest;
+use GRPC\Centrifugo\PublishResponse;
+use GRPC\Centrifugo\RefreshRequest;
+use GRPC\Centrifugo\RefreshResponse;
+use GRPC\Centrifugo\RPCRequest;
+use GRPC\Centrifugo\RPCResponse;
+use GRPC\Centrifugo\StreamSubscribeRequest;
+use GRPC\Centrifugo\StreamSubscribeResponse;
+use GRPC\Centrifugo\SubRefreshRequest;
+use GRPC\Centrifugo\SubRefreshResponse;
+use GRPC\Centrifugo\SubscribeRequest;
+use GRPC\Centrifugo\SubscribeResponse;
+use Spiral\RoadRunner\GRPC\ContextInterface;
+
+class CentrifugoProxy implements CentrifugoProxyInterface
+{
+    public function Connect(ContextInterface $ctx, ConnectRequest $in): ConnectResponse
+    {
+        return new ConnectResponse([
+            'allowed'   => true,
+            'expire_at' => time() + 3600,
+            'info'      => ['role' => 'user'],
+        ]);
+    }
+
+    public function Refresh(ContextInterface $ctx, RefreshRequest $in): RefreshResponse
+    {
+        return new RefreshResponse([]);
+    }
+
+    public function Subscribe(ContextInterface $ctx, SubscribeRequest $in): SubscribeResponse
+    {
+        $test = 0;
+        return new SubscribeResponse([]);
+    }
+
+    public function Publish(ContextInterface $ctx, PublishRequest $in): PublishResponse
+    {
+        return new PublishResponse([]);
+    }
+
+    public function RPC(ContextInterface $ctx, RPCRequest $in): RPCResponse
+    {
+        return new RPCResponse([
+            'data' => $in->getData(),
+        ]);
+    }
+
+    public function SubRefresh(ContextInterface $ctx, SubRefreshRequest $in): SubRefreshResponse
+    {
+        return new SubRefreshResponse([
+            'allowed'   => true,
+        ]);
+    }
+
+    public function SubscribeUnidirectional(
+        ContextInterface $ctx,
+        SubscribeRequest $in
+    ): StreamSubscribeResponse {
+        return new StreamSubscribeResponse([
+            'allowed' => true,
+        ]);
+    }
+
+    public function SubscribeBidirectional(
+        ContextInterface $ctx,
+        StreamSubscribeRequest $in
+    ): StreamSubscribeResponse {
+        return new StreamSubscribeResponse([
+            'allowed' => true,
+        ]);
+    }
+
+    public function NotifyCacheEmpty(
+        ContextInterface $ctx,
+        NotifyCacheEmptyRequest $in
+    ): NotifyCacheEmptyResponse {
+        return new NotifyCacheEmptyResponse([]);
+    }
+
+    public function NotifyChannelState(
+        ContextInterface $ctx,
+        NotifyChannelStateRequest $in
+    ): NotifyChannelStateResponse {
+        return new NotifyChannelStateResponse([]);
+    }
+}
---
+++ src/RrCentrifugoBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Rr\Bundle\Centrifugo;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RrCentrifugoBundle extends Bundle
+{
+
+}
